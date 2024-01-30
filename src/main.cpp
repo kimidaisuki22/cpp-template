7c32d50050c0ebe75351370f856c052e2b68d7ac
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2024-01-30 14:42:04 +0000

    add a module lib

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,14 @@
 #include <format>
 #include <iostream>
 #include <stacktrace>
+import foo;
 
 int main(){
 	std::cout << std::format("Hello {}\n", __cplusplus);
+
+	
+  foo f;
+  f.helloworld();
 	
 //    std::cout << std::stacktrace::current() << '\n';
   return 0;

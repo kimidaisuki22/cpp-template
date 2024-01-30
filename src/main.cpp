95dccc98c7f56b673e94cb57f35efabc1027973b
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2024-01-30 14:33:39 +0000

    use gcc-14 and mold

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include <main/main.h>
+#include <format>
+#include <iostream>
+#include <stacktrace>
 
 int main(){
+	std::cout << std::format("Hello {}\n", __cplusplus);
+	
+//    std::cout << std::stacktrace::current() << '\n';
   return 0;
 }
